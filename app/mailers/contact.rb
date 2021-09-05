43c7274d2cb484b8518f39692290b8173954b0b7
Tristan Orndoff <torndoff@icloud.com>
2021-09-04 22:42:39 -0500

    Changed contact form from HTML to embedded ruby.

--- app/mailers/contact.rb
+++ app/mailers/contact.rb
@@ -1,16 +1,17 @@
 class Contact < MailForm::Base
-    attribute :name,      :validate => true
-    attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-    attribute :message
-    attribute :nickname,  :captcha  => true
+    attribute :first_names,      :validate => true
+    attribute :last_names,       :validate => true
+    attribute :emails,    :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+    attribute :comments
+    attribute :phone_numbers,    :validate => /\\+*.\\s+\\([0-9]+*.-*.\\)/
   
     # Declare the e-mail headers. It accepts anything the mail method
     # in ActionMailer accepts.
     def headers
       {
         :subject => "Contact Form Inquiry",
-        :to => "your_email@your_domain.com",
-        :from => %("#{name}" <#{email}>)
+        :to => "mckenziebrower220@gmail.com",
+        :from => %("#{first_names}" + "#{last_names}" <#{email}>)
       }
     end
-  end
+end
