aa18b8abcdd1f1c4c16e053738a1c0ba13810aee
Tristan Orndoff <torndoff@icloud.com>
2021-09-05 17:52:11 -0500

    Added ActiveAdmin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
   root "welcome#index"
+  devise_for :users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  
 
   resources :contact, only: [:index, :new, :create]
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+AdminUser.create(:email => 'mckenziebrower220@gmail.com', :password => 'password123', :password_confirmation => 'password123')
