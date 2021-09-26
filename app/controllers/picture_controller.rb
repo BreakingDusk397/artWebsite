4f6e50b29a173ba3ce307f67925edcba15034c21
Tristan Orndoff <torndoff@icloud.com>
2021-09-26 14:55:32 -0500

    delete old devise and admin files

--- app/controllers/picture_controller.rb
+++
@@ -1,8 +0,0 @@
-class PictureController < ApplicationController
-
-
-    private
-        def picture_params
-            params.require(:photo).permit(:title, :description, :date, :location, :album_id, :image)
-        end    
-end
