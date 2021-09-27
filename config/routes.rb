4a3b870fd54225ab185652520b0c2b64d2bd6809
Tristan Orndoff <torndoff@icloud.com>
2021-09-27 17:07:57 -0500

    reorganized routes for better readability

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,30 @@
 Rails.application.routes.draw do
 
-  post "orders/create", to: "orders#create"
+  root "pages#home"
+  
   resources :webhooks, only: [:create]
+
+  post "orders/create", to: "orders#create"
   get "success", to: "orders#success"
   get "cancel", to: "orders#cancel"
-
+  resources :orders
 
   post "pictures/add_to_cart/:id", to: "pictures#add_to_cart", as: "add_to_cart"
   delete "pictures/remove_from_cart/:id", to: "pictures#remove_from_cart", as: "remove_from_cart"
   resources :pictures
-  root "pages#home"
+  
   get "home", to: "pages#home"
   get "bio", to: "pages#bio"
   get "portfolio", to: "pages#portfolio"
   get "contact", to: "pages#contact"
   post "contact", to: "pages#create"
-
   resources :pages
-  resources :users
+  
+  get '/dashboard', to: 'users#dashboard'
   get '/signup', to: 'users#new'
   get '/login', to: 'users#login'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  resources :users
 
 end  
